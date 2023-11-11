3526a4e333eb8100c86430acb6b2bc49e782d404
Jhenbert Villamucho <jvv205756@gmail.com>
2023-11-12 00:56:35 +0800

    fix env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import dotenv from "dotenv";
+import { defineConfig, loadEnv } from "vite";
 
-dotenv.config();
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    define: {
+      __APP_ENV__: JSON.stringify(env.VITE_API_KEY),
+    },
+  };
 });
