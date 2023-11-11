f5fc3f2fb9f78612bb3802e995469c5f6d5ab83c
Jhenbert Villamucho <jvv205756@gmail.com>
2023-11-12 01:03:37 +0800

    minor commit env

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [react()],
@@ -12,7 +14,7 @@ export default defineConfig(({ command, mode }) => {
       },
     },
     define: {
-      __APP_ENV__: JSON.stringify(env.VITE_API_KEY),
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
   };
 });
